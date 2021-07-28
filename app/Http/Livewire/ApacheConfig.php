53d1b9995fb1bbecd3f58f2fbc7ccacc858b91ae
Skino <itmike2018@gmail.com>
2021-07-28 22:40:17 +0100

    got the basics working on the page, just need to get the toggle working to show and hide the inputs based on the toggle input for SSL

---
+++ app/Http/Livewire/ApacheConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ApacheConfig extends Component
+{
+    
+    public $serverAdmin;
+    public $serverName;
+    public $serverAlias;
+    public $documentRoot;
+    public $certificateFile;
+    public $certificateKey;
+    public $certificateChain;
+
+    public function render()
+    {
+        return view('livewire.apache-config');
+    }
+}

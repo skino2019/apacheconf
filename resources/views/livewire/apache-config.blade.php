b321b71c4516d555c6a90eaacdeb8e65210a0d81
Mike Jones <skino@OptiPlex-7080-fd138b75.localdomain>
2021-08-21 18:35:48 +0100

    reformating so need to put changes to apacheconf on git

--- resources/views/livewire/apache-config.blade.php
+++ resources/views/livewire/apache-config.blade.php
@@ -55,36 +55,6 @@
 
                 @if ($showSsl)
 
-{{--                    <label for="my-modal-ssl" class="btn btn-secondary modal-button mt-4">Click here for SSL Certificate--}}
-{{--                        Information</label>--}}
-{{--                    <input type="checkbox" id="my-modal-ssl" class="modal-toggle">--}}
-{{--                    <div class="modal">--}}
-{{--                        <div class="modal-box">--}}
-
-{{--                            <h2 class="card-title">Enable SSL</h2>--}}
-{{--                            <p class="pb-6 text-justify">Before enabeling the config you need to first make sure SSL is--}}
-{{--                                working on your server by typing <span class="text-secondary">sudo a2enmod ssl</span> in--}}
-{{--                                your terminal</p>--}}
-
-{{--                            <h2 class="card-title">Enable mod_rewrite</h2>--}}
-{{--                            <p class="pb-6 text-justify">Before enabeling the config you need to also make sure rewrite--}}
-{{--                                is working on your server by typing <span class="text-secondary">sudo a2enmod--}}
-{{--                                    rewrite</span> in your terminal</p>--}}
-
-{{--                            <h2 class="card-title">Certificate Location</h2>--}}
-{{--                            <p class="pb-3.5 text-justify">The SSL Certificates can be stored anywhere on the system,--}}
-{{--                                however you will need to ensure the right user priviges are supplied so Apache2 can get--}}
-{{--                                to them.</p>--}}
-{{--                            <p class="text-justify">As standard the location for certificates (On ubuntu) is--}}
-{{--                                '/usr/local/share/ca-certificates/', and id recommend using this location for ease.</p>--}}
-{{--                            <div class="modal-action">--}}
-{{--                                <label for="my-modal-ssl" class="btn btn-secondary">Close Modal</label>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-
-
                     {{-- SSL Info --}}
                     <label class="label">
                         <span class="label-text">SSLCertificateFile</span>
@@ -109,8 +79,6 @@
                 @endif
             </div>
 
-
-
             <div class="flex flex-around">
                 <button onclick="copyToClipboard('#content-copy ~ pre > code')"
                     class="flex flex-auto btn bg-secondary hover:bg-secondary-focus modal-button mt-3">Copy to
@@ -118,12 +86,7 @@
 
             </div>
 
-
-{{--    test    --}}
-        <!-- component -->
             <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-
-
 
                 <!-- Modal -->
                 <div x-data="{ showModal : false }" class="flex flex-auto">
@@ -214,57 +177,8 @@
                     </div>
                 </div>
 
-{{--    test    --}}
-
-{{--            <label for="my-modal-htaccess"--}}
-{{--                class="flex flex-auto btn bg-secondary hover:bg-secondary-focus modal-button mt-3">.htaccess--}}
-{{--                Information</label>--}}
-{{--            <input type="checkbox" id="my-modal-htaccess" class="modal-toggle">--}}
-{{--            <div class="modal">--}}
-{{--                <div class="modal-box">--}}
-{{--                    <h2 class="card-title">HTACCESS Information</h2>--}}
-{{--                    <p class="pb-6 text-justify">In your Laravel Project you will find the HTACCESS file under <span--}}
-{{--                            class="text-white font-bold">/public/.htaccess</span><br><br>--}}
-{{--                        Replace the existing Laravel .htaccess content with the information below. this will make sure--}}
-{{--                        the connections are all correct.--}}
-{{--                    </p>--}}
-{{--                    <div class="text-xs">--}}
-{{--                        <pre>--}}
-{{--<code class="text-xs text-white font-semibold">--}}
-
-{{--&lt;IfModule mod_rewrite.c&gt;--}}
-{{--    &lt;IfModule mod_negotiation.c&gt;--}}
-{{--        Options -MultiViews -Indexes--}}
-{{--    &lt;/IfModule&gt;--}}
-
-{{--    RewriteEngine On--}}
-
-{{--    # Handle Authorization Header--}}
-{{--    RewriteCond %{HTTP:Authorization} .--}}
-{{--    RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]--}}
-
-{{--    # Redirect Trailing Slashes If Not A Folder...--}}
-{{--    RewriteCond %{REQUEST_FILENAME} !-d--}}
-{{--    RewriteCond %{REQUEST_URI} (.+)/$--}}
-{{--    RewriteRule ^ %1 [L,R=301]--}}
-
-{{--    # Send Requests To Front Controller...--}}
-{{--    RewriteCond %{REQUEST_FILENAME} !-d--}}
-{{--    RewriteCond %{REQUEST_FILENAME} !-f--}}
-{{--    RewriteRule ^ index.php [L]--}}
-{{--&lt;/IfModule&gt;--}}
-
-{{--</code>--}}
-{{--</pre>--}}
-{{--                    </div>--}}
-{{--                    <div class="modal-action">--}}
-{{--                        <label for="my-modal-htaccess" class="btn btn-secondary">Close Modal</label>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
         </div>
     </div>
-    {{-- <div class="divider divider-vertical divide-base-100"></div> --}}
     <div id='post-title' class="grid flex-grow w-2/3 card bg-base-200 rounded-box place-items-start ml-4 text-md">
 
         <textarea id="content-copy" class="hidden plain-text">
